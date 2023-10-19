b76e7081318825836b537b224663d535b5ff0155
Axcell Hernandez <axcellhernandez15@gmail.com>
2023-10-18 23:33:21 -0400

    primer change

---
+++ Ejercicio 1/Ejercicio/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        CalcularPerimetroCuadrado();
+        CalcularSumaYProducto();
+        CalcularSumaYPromedio();
+    }
+
+    static void CalcularPerimetroCuadrado()
+    {
+        Console.WriteLine("Ingrese el lado del cuadrado:");
+        int lado = Convert.ToInt32(Console.ReadLine());
+        int perimetro = lado * 4;
+        Console.WriteLine($"El perímetro del cuadrado es: {perimetro}");
+    }
+
+    static void CalcularSumaYProducto()
+    {
+        Console.WriteLine("Ingrese cuatro números separados por espacios:");
+        string[] numeros = Console.ReadLine().Split(' ');
+
+        if (numeros.Length != 4)
+        {
+            Console.WriteLine("Debe ingresar exactamente cuatro números.");
+            return;
+        }
+
+        int num1 = Convert.ToInt32(numeros[0]);
+        int num2 = Convert.ToInt32(numeros[1]);
+        int num3 = Convert.ToInt32(numeros[2]);
+        int num4 = Convert.ToInt32(numeros[3]);
+
+        int suma = num1 + num2;
+        int producto = num3 * num4;
+
+        Console.WriteLine($"La suma de los dos primeros números es: {suma}");
+        Console.WriteLine($"El producto del tercer y cuarto número es: {producto}");
+    }
+
+    static void CalcularSumaYPromedio()
+    {
+        Console.WriteLine("Ingrese cuatro números separados por espacios:");
+        string[] numeros = Console.ReadLine().Split('');
+
+        if (numeros.Length != 4)
+        {
+            Console.WriteLine("Debe ingresar exactamente cuatro números.");
+            return;
+        }
+
+        int num1 = Convert.ToInt32(numeros[0]);
+        int num2 = Convert.ToInt32(numeros[1]);
+        int num3 = Convert.ToInt32(numeros[2]);
+        int num4 = Convert.ToInt32(numeros[3]);
+
+        int suma = num1 + num2;
+        double promedio = (num1 + num2 + num3 + num4) / 4.0;
+
+        Console.WriteLine($"La suma de los cuatro números es: {suma}");
+        Console.WriteLine($"El promedio de los cuatro números es: {promedio}");
+    }
+}
+
---
+++ Practica #1/Practica #1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Ingrese la Fecha de tu cumple:");
+
+int Mes = 0;
+int Dia = 0;
+int Año = 0 ;
+string linea = string.Empty;
+
+//Pedir numeros //
+Console.WriteLine("Ingrese el valor de Mes: ");
+linea = Console.ReadLine();
+Mes = Convert.ToInt32(linea);
+
+Console.WriteLine("Ingrese el valor de Dia: ");
+linea = Console.ReadLine();
+Dia = Convert.ToInt32(linea);
+
+Console.WriteLine("Ingrese el valor de Año: ");
+linea = Console.ReadLine();
+Año = Convert.ToInt32(linea);
+
+if(Mes == 1 || Mes == 2 || Mes == 3)
+
+{
+    Console.WriteLine("El mes corresponde al primer trimestre");
+}
+else
+{
+    Console.WriteLine("El mes no corresponde al primer trimestre");
+}

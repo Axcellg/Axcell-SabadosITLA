6c500a4b9e0734afe202bb77e4515eb0ff1e94b0
Axcellg <axcellhernandez15@gmail.com>
2023-12-05 15:53:46 -0400

    next

--- Clase c#/Ejercicios Aplicando Bucles/Program.cs
+++ Clase c#/Ejercicios Aplicando Bucles/Program.cs
@@ -4,4 +4,20 @@ a) De cada triángulo la medida de su base, su altura y su superficie.
 b) La cantidad de triángulos cuya superficie es mayor a 12.
 Desarrollar un programa que solicite la carga de 10 números e imprima la suma de los últimos 5 valores ingresados.*/
 
-public 
+
+    MedidasdeunTriangulo();
+
+    void MedidasdeunTriangulo()
+    {
+
+    int Altura = 0;
+    int Base = 0;
+    int Superficie = 0;
+    int Triangulo = 0;
+    string linea = string.Empty;
+
+
+
+
+    }
+

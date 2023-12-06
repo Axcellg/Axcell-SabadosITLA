004cde86e73939ffb2ba76d4b5627a01f90ac7ba
Axcellg <axcellhernandez15@gmail.com>
2023-12-06 15:58:37 -0400

    HOY

---
+++ Clase c#/Ejercicio 1/test.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		/*Confeccionar un programa que lea n pares de datos, cada par de datos corresponde a la medida de la base y la altura de un triángulo.
+ El programa deberá informar:
+a) De cada triángulo la medida de su base, su altura y su superficie.
+b) La cantidad de triángulos cuya superficie es mayor a 12.
+Desarrollar un programa que solicite la carga de 10 números e imprima la suma de los últimos 5 valores ingresados.*/
+
+
+internal class test
+
+    {
+        private static void Main(string[] args)
+        {
+            CalcularPromedio();
+
+
+            void CalcularPromedio()
+            {
+                int suma = 0;
+                int valor = 0;
+                decimal promedio = 0;
+                int cantidad = 0;
+                string linea = string.Empty;
+                int x = 1;
+
+
+                Console.WriteLine("Ingrese la cantidad: ");
+                linea = Console.ReadLine();
+
+                if (int.TryParse(linea, out int myCantidad))
+                {
+                    cantidad = myCantidad;
+                }
+                else
+                {
+                    Console.WriteLine("La cantidad ingresada no es válida");
+                    return;
+                }
+
+                Console.WriteLine("---------------------------INGRESAR LOS VALORES------------------------------");
+
+                while (x <= cantidad)
+                {
+                    Console.WriteLine($"Ingrese el valor {x} ");
+                    linea = Console.ReadLine();
+
+
+                    if (int.TryParse(linea, out int myValue))
+                    {
+                        valor = myValue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El valor digitado es inválido");
+                        break;
+                    }
+
+
+                    suma = suma + valor;
+                    x += 1;
+                }
+
+                promedio = suma / cantidad;
+
+                Console.WriteLine("--------------------------------------RESULTADOS----------------------------------------------------");
+
+
+                Console.WriteLine($"La suma es: {suma} ");
+
+                Console.WriteLine($"El promedio es: {promedio}");
+
+
+            }
+        }
+    }
+}
+}
---
+++ Clase c#/Ejercicio aplicando bucles/Bucles/Bucles/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Confeccionar un programa que lea n pares de datos, cada par de datos corresponde a la medida de la base y la altura de un triángulo.
+ El programa deberá informar:
+a) De cada triángulo la medida de su base, su altura y su superficie.
+b) La cantidad de triángulos cuya superficie es mayor a 12.
+Desarrollar un programa que solicite la carga de 10 números e imprima la suma de los últimos 5 valores ingresados.*/
+
+
+using System.Drawing;
+
+public class Program2
+{
+    private static void Main(string[] args)
+    {
+
+        MedidasdeunTriangulo();
+
+        void MedidasdeunTriangulo()
+
+        {
+
+            int Altura = 0;
+            int Base = 0;
+            int Superficie = 0;
+            int Triangulo = 1;
+            int cantidad = 0;
+            string linea = string.Empty;
+
+            Console.WriteLine("Ingrese la cantidad de triangulos : ");
+            linea = Console.ReadLine();
+            Triangulo = Convert.ToInt32(linea);
+
+            if (int.TryParse(linea, out int mycantidad))
+            {
+                cantidad = mycantidad;
+            }
+
+            else
+            {
+                Console.WriteLine("El triangulo no es valido");
+                return;
+            }
+
+            Console.WriteLine("---------------------------INGRESAR LAS MEDIDAS DEL TRIANGULO------------------------------");
+
+            while (Triangulo <= cantidad)
+            {
+                Console.WriteLine($"     LAS MEDIDAS DEL TRIANGULO {Triangulo}: ");
+
+                Console.WriteLine($"Ingrese la base del triangulo {Triangulo}: ");
+                linea = Console.ReadLine();
+                Base = Convert.ToInt32(linea);
+
+
+
+                if (int.TryParse(linea, out int myValue))
+                {
+                    Base = myValue;
+                }
+                else
+                {
+                    Console.WriteLine("El valor digitado es inválido");
+                    break;
+                }
+
+                Triangulo += 1;
+
+
+
+
+            }
+        }
+    }
+}
+    

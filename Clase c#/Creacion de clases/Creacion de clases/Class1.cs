68412ebc04e74a2732f4028d8b40d8801cc8c3f2
Axcellg <axcellhernandez15@gmail.com>
2023-12-07 15:58:50 -0400

    pam

--- Clase c#/Creacion de clases/Creacion de clases/Class1.cs
+++ Clase c#/Creacion de clases/Creacion de clases/Class1.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Creacion_de_clases
 {
-    public class Class1
+    public class Operaciones
     {
         /*1.Confeccionar una clase que permita carga el nombre y la edad de una persona
          * .Mostrar los datos cargados.Imprimir un mensaje si es mayor de edad (edad>=18).
@@ -11,6 +11,28 @@ namespace Creacion_de_clases
        3.Implementar la clase operaciones.Se deben cargar dos valores enteros, 
         calcular su suma, resta, multiplicación y división, cada una en un método, 
         imprimir dichos resultados.*/
+
+       public int Valor1 { get; set; }
+       public int Valor2 { get; set;}
+       public int Suma { get; set;}
+       public int Resta { get; set;}
+       public int Multiplicacion { get; set;}
+       public int Division { get; set;}
+
+        public void CargadeValores()
+        {
+            Console.WriteLine("Ingresa el valor 1:");
+            Valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa el Valor 2:");
+            Valor2 = int.Parse(Console.ReadLine());
+        }
+
+        public void Suma()
+        {
+
+        }
+
 
     }
 }
---
+++ Clase c#/Ejercicio 1/Ejercicios/Program.cs
@@ -0,0 +1,82 @@
+﻿public class Program
+
+{
+    private static void Main(string[] args)
+    {
+        CalcularPerimetroCuadrado();
+        CalcularSumaYProducto();
+        CalcularSumaYPromedio();
+
+        void CalcularPerimetroCuadrado()
+
+        {
+
+            int lado;
+            int perimetro;
+
+            Console.WriteLine("-------------------------------------Ingrese el lado del cuadrado:-------------------------------------------------");
+            lado = Convert.ToInt32(Console.ReadLine());
+
+            perimetro = lado * 4;
+
+            Console.WriteLine($"El perímetro del cuadrado es: {perimetro}");
+
+        }
+
+        void CalcularSumaYProducto()
+
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int num4 = 0;
+            int suma;
+            int producto;
+
+
+
+            Console.WriteLine("--------------------------------------Ingrese cuatro números:----------------------------------------------------------");
+             num1 = Convert.ToInt32(Console.ReadLine());
+             num2 = Convert.ToInt32(Console.ReadLine());
+             num3 = Convert.ToInt32(Console.ReadLine());
+             num4 = Convert.ToInt32(Console.ReadLine());
+
+             suma = num1 + num2;
+
+             producto = num3 * num4;
+
+            Console.WriteLine($"La suma de los dos primeros números es: {suma}");
+
+            Console.WriteLine($"El producto del tercer y cuarto número es: {producto}");
+        }
+
+
+        void CalcularSumaYPromedio()
+
+        {
+            int valor1 = 0;
+            int valor2 = 0;
+            int valor3 = 0;
+            int valor4 = 0;
+            int Suma;
+            int promedio;
+
+            Console.WriteLine("-----------------------------------------Ingrese cuatro valores numéricos:-------------------------------------------------");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+            valor2 = Convert.ToInt32(Console.ReadLine());
+            valor3 = Convert.ToInt32(Console.ReadLine());
+            valor4 = Convert.ToInt32(Console.ReadLine());
+
+            Suma = valor1 + valor2 + valor3 + valor4;
+
+            promedio = Suma / 4;
+
+
+            Console.WriteLine($"La suma de los cuatro valores es: {Suma}");
+
+            Console.WriteLine($"El promedio de los cuatro valores es: {promedio}");
+        }
+
+
+    }
+}
